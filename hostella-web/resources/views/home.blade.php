75c7961f9a1ffbf024c3daf03fb6ece529c484c5
michael.cardenas <michael.cardenas@realtechltda.com>
2025-03-24 12:42:52 -0500

    subir images

--- hostella-web/resources/views/home.blade.php
+++ hostella-web/resources/views/home.blade.php
@@ -163,7 +163,7 @@
 
                             @if (!empty($pagina->$imageField))
                             <img 
-                            src="{{ asset($pagina->$imageField) }}"
+                            src="{{ asset(images/$pagina->$imageField) }}"
 
                                 alt="Imagen tarjeta {{ $i }}" 
                                 class="mb-4 img-fluid" 
